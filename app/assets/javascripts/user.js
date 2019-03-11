0af56b565b902e9f1bc9e39a7edc9d6c496a84f6
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-03-11 15:58:28 +0900

    Edit user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,18 +5,17 @@ $(function(){
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>
                   `
-    return html
+    return html;
   }
 
   function removeGroupUser(user_id,user_name){
-    var html=`<div class='chat-group-user clearfix' id='chat-group-user-22'>
-  }
-  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-  <p class='chat-group-user__name'>${user_name}</p>
-  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-</div>
-    `
-    return html
+    var html = `<div class='chat-group-user clearfix' id='chat-group-user-22'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>
+               `
+    return html;
   }
   $(document).on('click',".chat-group-user__btn--add",function(){
     var user_id = $(this).attr('data-user-id');
