a5fb4eca0a8f2bd6d252633cdb100eee0770e123
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-03-12 21:29:28 +0900

    Edit messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)
     respond_to do |format|
       format.html
-      format.json { @new_messages = @messages.where('id > ?' , params[:lastMessageId])}
+      format.json { @new_messages = @messages.where('id > ?' , params[:last_message_id])}
     end
   end
 
