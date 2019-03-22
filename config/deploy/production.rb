78e54cf200f0f2880d130c600eda034c3faf1951
TakumiKATAOKA <kataokatakumi@kataokatakuhitsujinoMacBook-Air.local>
2019-03-22 17:39:31 +0900

    Commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.196.5.89', user: 'ec2-user', roles: %w{app db web}
+server '52.69.216.118', user: 'ec2-user', roles: %w{app db web}
